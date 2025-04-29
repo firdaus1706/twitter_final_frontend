423799e48760511fb575f83e05b22ec397dd5f33
Firdaus Shaikh <shaikhfirdaus17@gmail.com>
2025-04-29 14:52:28 +0530

    second repo

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,2 +1,5 @@
-export const USER_API_END_POINT = "http://localhost:8000/api/v1/user";
-export const TWEET_API_END_POINT = "http://localhost:8000/api/v1/tweet";
+// export const USER_API_END_POINT = "http://localhost:8000/api/v1/user";
+// export const TWEET_API_END_POINT = "http://localhost:8000/api/v1/tweet";
+
+export const USER_API_END_POINT = "https://twitter-final-backend.onrender.com/api/v1/user";
+export const TWEET_API_END_POINT = "https://twitter-final-backend.onrender.com/api/v1/tweet";
